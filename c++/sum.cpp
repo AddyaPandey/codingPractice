dd2c64fd255591fca5d0d5ae9abbfef6afaa5992
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-04-07 14:22:54 +0530

    a

---
+++ c++/sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int x;
+cout<<"enter first number : ";
+cin>>x;
+cout<<endl;
+
+int y;
+cout<<"enter second number : ";
+cin>>y;
+cout<<endl;
+
+cout<<"sum of these num5bers are:" <<endl;
+ cout<<x+y;
+
+}
