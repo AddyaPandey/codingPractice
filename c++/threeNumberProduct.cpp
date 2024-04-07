d5427ecacbd2ccdebace73e1a1340dae110d7e54
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-04-07 14:33:43 +0530

    b

---
+++ c++/threeNumberProduct.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int x;
+cout<<"enter first number : ";
+cin>>x;
+cout<<endl;
+
+int y;
+cout<<"enter second number : ";
+cin>>y;
+cout<<endl;
+
+int z;
+cout<<"enter 3rd number : ";
+cin>>z;
+cout<<endl;
+
+cout<<"product of these numbers are:" <<endl;
+ cout<<x*y*z;
+
+}
