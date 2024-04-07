f4ad828296eeb9ab5959adf72ec1097785661c52
Addya Pandey <123183433+AddyaPandey@users.noreply.github.com>
2024-04-07 14:15:02 +0530

    x

---
+++ c++/squareOfNumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int x = 4.3;
+
+cout<<"enter a number to find its square : ";
+cin>>x;
+cout<<endl;
+cout<<x*x;
+
+ // square of a number cin and cout
+}
